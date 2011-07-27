31b1b1607558d2e3b68814cbb2d4174a0bfe42de
leshiy <delphi7@list.ru>
2011-07-27 15:34:13 +0400

    crsf

--- board/views.py
+++ board/views.py
@@ -1,18 +1,21 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from cards.board.forms import SaveWord
-from django.core.context_processors import csrf
+
 
 
 def test(request):
-    c = {}
-    c.update(csrf(request))
-
     if request.method=="POST":
         form=SaveWord(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            print cd['translate']
+            print cd['word']
+            print cd['value']
+        
     else:
         form=SaveWord()
 
-    return render_to_response('index.html',{'form':form,'c':c})
+    return render_to_response('index.html',locals())
 
 
