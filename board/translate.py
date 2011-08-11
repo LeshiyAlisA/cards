391075be0a70df69eac74d601d7cb573757212ed
leshiy <delphi7@list.ru>
2011-08-11 17:17:23 +0400

    итог на сегодня

--- board/translate.py
+++ board/translate.py
@@ -2,17 +2,12 @@
 __author__ = 'leshiy'
 import urllib
 import html5lib
-from html5lib import treebuilders
-from xml.etree import cElementTree
-from lxml import etree
 from html5lib import treebuilders, sanitizer
 
 
 class translate:
 
-    def test(self,word):
-        url1='http://lingvo.yandex.ru/table/%D1%81%20%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B3%D0%BE/'
-        url2='http://lingvo.yandex.ru/'+word+'/%D1%81%20%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B3%D0%BE/'
+    def ValueTranscription(self,word):
 
         u=urllib.urlopen('http://lingvo.yandex.ru/'+word+'/%D1%81%20%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B3%D0%BE/')
         str=u.read()
@@ -25,20 +20,13 @@ class translate:
                     
                     for child in elem.childNodes:
                         if child.nodeType==3:
-                            print child.data.strip()
+                            value = child.data.strip()
 
         for elem in dom.getElementsByTagName('span'):
             if elem.hasAttribute('class'):
                 if elem.getAttribute('class')=='b-translate__tr':
                     for child in elem.childNodes:
                         if child.nodeType==3:
-                            print child.data.strip()
+                            transcription = child.data.strip()
 
-
-
-
-      
-
-
-
-
+        return {'value':value,'transcription':transcription}
