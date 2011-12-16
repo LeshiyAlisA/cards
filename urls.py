398a9b5ed97801cc7ce173c589b43f57bef31ef7
leshiy <leshiy@hpbook.(none)>
2011-12-17 01:12:18 +0400

    привожу в порядок

--- urls.py
+++ urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
-     url('^$', main),
+     url('^$', AddWords),
      url(r'^addwords/', AddWords),
      url(r'^accounts/login/$',  login),
      url(r'^accounts/logout/$', logout),
